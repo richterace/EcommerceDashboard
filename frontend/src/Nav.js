62cf01c720cca26b4b709950389769562286f40b
Richter Ace G. Villas <villasace098@gmail.com>
2024-12-21 15:16:29 +0800

    installing router

---
+++ frontend/src/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+    return (
+        <div>
+            <ul class="nav-ul">
+                <li><Link to="/">Products</Link></li>
+                <li><Link to="/add"> Add Products</Link></li>
+                <li><Link to="/update">Update Products</Link></li>
+                <li><Link to="/logout">Logout</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+            </ul>
+        </div>
+    )
+}
+
+// we need to export it
+export default Nav;
