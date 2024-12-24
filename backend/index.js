6d3b901e332bf0810dc23d7a0e7cd01838f25a0f
Richter Ace G. Villas <villasace098@gmail.com>
2024-12-24 16:47:42 +0800

    initial database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
-app.get("/", (req, res) => {
-    res.send("app is working...")
-})
+const connectDB = async () => {
+    mongoose.connect("mongodb://localhost:27017//e-comm") // initiate the host
+    const productSchema = new mongoose.Schema({}); // initialize the productschema
+    const product = mongoose.model("products", productSchema); //our collection name
+    const data = await product.find(); // we can only use await inside async function
+    console.warn(data);
+}
+
+connectDB();
+
 
 app.listen(5000);
