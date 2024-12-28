8c65d464950b8f573be8457ee2495ffb8559f349
Richter Ace G. Villas <villasace098@gmail.com>
2024-12-28 12:47:35 +0800

    Login API integration

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const handleLogin = () => {
-        console.warn(email, password)
+    const navigate = useNavigate();
+    useEffect(() => {
+        const auth = localStorage.getItem('user')
+        if (auth) {
+            navigate('/')
+        }
+    })
+    const handleLogin = async () => {
+        let result = await fetch("http://localhost:5000/login", {
+            method: 'post',
+            body: JSON.stringify({ email, password }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        result = await result.json();
+        console.warn(result)
+        if (result.name) {
+            localStorage.setItem("user", JSON.stringify(result))
+            navigate('/')
+        } else {
+            alert('Please enter valid details.')
+        }
+
     }
     return (
         <div className="login">
@@ -15,7 +38,7 @@ const Login = () => {
                     onChange={(e) => setEmail(e.target.value)} value={email} />
                 <input className="inputBox" type="password" placeholder="Enter password"
                     onChange={(e) => setPassword(e.target.value)} value={password} />
-                <button onClick={handleLogin} classname="LoginButton" type="button">Login</button>
+                <button onClick={handleLogin} className="LoginButton" type="button">Login</button>
             </h1>
         </div>
 
