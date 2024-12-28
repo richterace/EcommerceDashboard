9ea3488bf06c64c3a4f32191dcf71583f73c98c4
Richter Ace G. Villas <villasace098@gmail.com>
2024-12-28 11:46:56 +0800

    log in api

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,28 @@ app.use(cors());
 app.post("/register", async (req, resp) => {
     let user = new User(req.body);
     let result = await user.save();
+    result = result.toObject();
+    delete result.password
     console.log("Request Body:", req.body);
     resp.send(result) // to check in postman if its working properly by sending specifics in body
 });  //two parameters this is coming from express for api, accepts path
+
+//api route for login
+app.post("/login", async (req, resp) => {
+
+    if (req.body.password && req.body.email) {
+        let user = await User.findOne(req.body).select("-password");
+        if (user) {
+            resp.send(user); // Send the user details if found
+        } else {
+            resp.send({ result: "User not found" }); // Send error message if not found
+        }
+    }
+    else {
+        resp.send({ result: "No user found" })
+    }
+
+});
 
 app.listen(5000);
 
