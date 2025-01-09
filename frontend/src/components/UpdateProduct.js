9bbfbc26d21f1bf0c021d67b6ea7016c2e950c08
Richter Ace G. Villas <villasace098@gmail.com>
2025-01-09 22:57:44 +0800

    put method update api

--- frontend/src/components/UpdateProduct.js
+++ frontend/src/components/UpdateProduct.js
@@ -1,34 +1,16 @@
-import React, { useEffect ,useState } from "react";
-import {useParams} from 'react-router-dom';
+import React, { useState } from "react";
+
 
 const UpdateProduct = () => {
     const [name, updateName] = useState();
     const [price, updatePrice] = useState();
     const [category, updateCategory] = useState();
     const [company, updateCompany] = useState();
-    const params = useParams();
 
     const UpdateProduct = () => {
         console.warn(name, price, category, company)
     }
 
-    useEffect(()=>{
-        
-        getProductDetails();
-
-    },[])
-
-    const getProductDetails= async ()=>{
-
-        console.warn(params);
-        let result = await fetch(`http://localhost:5000/product/${params.id}`);
-        result = await result.json();
-        console.warn(result)
-        updateName(result.name);
-        updatePrice(result.price);
-        updateCategory(result.category);
-        updateCompany(result.company);
-    }
 
     return (
         <div className="UpdateProduct">
