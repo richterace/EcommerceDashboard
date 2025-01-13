834cc9b0f04d476e698755dbba6d1b97ff852d34
richterace <villasace098@gmail.com>
2025-01-13 13:03:59 +0800

    Search integrated on React

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -40,10 +40,31 @@ const ProductList = () => {
         }
 
     }
+
+    const searchHandle = async (event) => {
+        let key = event.target.value;
+        if(key){
+
+        
+        let result = await fetch(`http://localhost:5000/search/${key}`);
+        result = await result.json()
+
+        if (result){
+            setProducts(result)
+        }
+    }
+        else{
+            getProducts();
+        }
+    }
+
     return (
         <div className="prodList">
             <h3>
                 Product List
+                <input type="" className='search-product-box' placeholder='Search Product'
+                onChange={searchHandle}
+                />
                 <ul>
                     <li>S. No</li>
                     <li>Name</li>
@@ -53,7 +74,7 @@ const ProductList = () => {
                     <li>Operation</li>
                 </ul>
                 {
-                    product.map((items, index) =>
+                    product.length > 0 ? product.map((items, index) =>
                         <ul key={items._id}>
                             <li>{index + 1}</li>
                             <li>{items.name}</li>
@@ -63,7 +84,10 @@ const ProductList = () => {
                             <li><button onClick={() => deleteProduct(items._id)}>Delete</button>
                                 <Link to={"/update/" + items._id} >  Update</Link>
                             </li>
-                        </ul>)
+                        </ul>
+                        )
+                        :
+                        <h1>No Result Found</h1>
                 }
             </h3>
         </div >
