b32f1970321f7cb8a3c6de88388ed29853dc9fa5
Richter Ace G. Villas <villasace098@gmail.com>
2025-07-15 16:06:32 +0800

    dang

--- backend/db/config.js
+++ backend/db/config.js
@@ -8,11 +8,3 @@ mongoose.connect("mongodb://localhost:27017/e-commerce", {
     .catch((err) => console.error("Could not connect to MongoDB", err));
 
 module.exports = mongoose;
-
-// const mongoose = require("mongoose");
-
-// mongoose.connect("mongodb+srv://admin:Aceaceace098@cluster0.rmyqc.mongodb.net/ecommerce?retryWrites=true&w=majority", {})
-//     .then(() => console.log("Connected to MongoDB"))
-//     .catch((err) => console.error("Could not connect to MongoDB", err));
-
-// module.exports = mongoose;
