10206d3dca2c1e2aee1fec6a891dc28beb1ecb8a
Richter Ace G. Villas <villasace098@gmail.com>
2025-07-15 16:12:47 +0800

    dang

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,10 +1,16 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/e-commerce", {
+// mongoose.connect("mongodb://localhost:27017/e-commerce", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+//     .then(() => console.log("Connected to MongoDB"))
+//     .catch((err) => console.error("Could not connect to MongoDB", err));
+
+// module.exports = mongoose;
+
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.error("Could not connect to MongoDB", err));
-
-module.exports = mongoose;
+});
